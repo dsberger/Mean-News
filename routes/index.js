1153bd7f92e676df2219b1c4ea8f2afbd193b2d8
Dan Berger <dsberger@gmail.com>
2016-02-23 14:12:51 -0800

    adds passport and User model to routes/index

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
+var passport = require('passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
