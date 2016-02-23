50a7448c188e02973d9190f69cd67ddb809486bc
Dan Berger <dsberger@gmail.com>
2016-02-23 14:06:10 -0800

    adds passport and user model to app.js

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ var bodyParser = require('body-parser');
 
 // Mongoose DB
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -26,6 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
@@ -62,6 +66,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
