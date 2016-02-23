7285d80fdca653ebd0b5f6b13167f5047e42c04f
Dan Berger <dsberger@gmail.com>
2016-02-23 15:40:45 -0800

    adds navbar and NavCtrl

---
+++ public/javascripts/controllers/NavCtrl.js
@@ -0,0 +1,6 @@
+app.controller('NavCtrl', [
+  '$scope', 'auth', function($scope, auth){
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
+  }]);
