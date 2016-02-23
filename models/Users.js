61e91e683ec938299c70b1670e21d9e319221d65
Dan Berger <dsberger@gmail.com>
2016-02-23 13:50:19 -0800

    adds generateJWT function to User model

--- models/Users.js
+++ models/Users.js
@@ -20,4 +20,17 @@ UserSchema.methods.validPassword = function(password){
   return this.hash === hash;
 };
 
+UserSchema.methods.generateJWT = function(){
+  // set expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000)
+  }, 'SECRET');
+};
+
 mongoose.model('User', UserSchema);
