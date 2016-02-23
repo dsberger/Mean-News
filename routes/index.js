4de76a1ce552aadac961000d57481083c2c878af
Dan Berger <dsberger@gmail.com>
2016-02-23 14:20:41 -0800

    adds user login route

--- routes/index.js
+++ routes/index.js
@@ -121,4 +121,21 @@ router.post('/register', function(req, res, next){
   });
 });
 
+/* login users */
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
 module.exports = router;
