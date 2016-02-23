bea5371f8171a1bd21ff654272f232dc4f946f3f
Dan Berger <dsberger@gmail.com>
2016-02-22 21:59:37 -0800

    adds param function to load posts by ID

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,16 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function( err, post ){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
+});
+
 module.exports = router;
