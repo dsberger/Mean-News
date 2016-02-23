8af3cffe27df880cdff0d1381c145abffff074b4
Dan Berger <dsberger@gmail.com>
2016-02-22 22:58:48 -0800

    adds some Comment paths and upvote method

--- routes/index.js
+++ routes/index.js
@@ -40,8 +40,12 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
-router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+router.get('/posts/:post', function(req, res, next) {
+  res.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
@@ -51,4 +55,41 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     res.json(post);
   });
 });
+
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if (err) { return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function( err, comment ){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
+
+    req.comment = comment;
+    return next();
+  })
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
 module.exports = router;
