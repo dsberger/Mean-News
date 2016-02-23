d32ba5e6cbbdf29533820b79dd3eb09d979641e7
Dan Berger <dsberger@gmail.com>
2016-02-23 13:38:45 -0800

    has partially complete User model

---
+++ models/Users.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  hash: String,
+  salt: String
+});
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  return this.hash === hash;
+};
+
+mongoose.model('User', UserSchema);
